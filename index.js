c152bcc59a8d1f66c5a4d699194329df97e67470
Oscar Ore <oscar.ore2011@gmail.com>
2022-01-06 09:42:34 -0500

    changed innerText to textContent to fix spacing issue

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 // document.getElementById("count").innerText = 5
 //camelCase 
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
     
-    count = count + 1 
-    countEl.innerText = count 
+    count += 1 
+    countEl.textContent = count 
    
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0 
 }
 
 save()
