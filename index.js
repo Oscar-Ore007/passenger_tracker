45b803b4836e579db74c7dc08df497aa387c6a1b
Oscar Ore <oscar.ore2011@gmail.com>
2022-01-06 08:07:48 -0500

    created save button

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // document.getElementById("count").innerText = 5
+//camelCase 
+let countEl = document.getElementById("count-el")
+
+let count = 0
 
 function increment() {
-    console.log("The button was clicked!")
+    
+    count = count + 1 
+    countEl.innerText = count 
+   
 }
+
+function save() {
+    console.log(count)
+}
+
+save()
